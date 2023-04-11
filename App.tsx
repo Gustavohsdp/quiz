da66283302ce6e8b7fdd6d17d1f1871b479c1957
Gustavo Henrique <gustavohsdp@gmail.com>
2023-04-11 20:57:35 -0300

    feat: :heavy_plus_sign: add react-native-gesture-handler

--- App.tsx
+++ App.tsx
@@ -1,26 +1,31 @@
-import { StatusBar } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import "react-native-gesture-handler";
 
-import { Routes } from './src/routes';
-import { Loading } from './src/components/Loading';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { StatusBar } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import { Loading } from "./src/components/Loading";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   if (!fontsLoaded) {
-    return (
-      <Loading />
-    );
+    return <Loading />;
   }
 
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Routes />
-    </>
+    </GestureHandlerRootView>
   );
 }
