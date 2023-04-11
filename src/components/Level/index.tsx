986c6465136c1c232b5fd6be0b7ae57260f8d38c
Gustavo Henrique <gustavohsdp@gmail.com>
2023-04-11 15:48:49 -0300

    feat: :dizzy: Apply animations on the level component

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,40 +1,80 @@
-import { TouchableOpacity, TouchableOpacityProps, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { Pressable, PressableProps } from "react-native";
 
-import { THEME } from '../../styles/theme';
-import { styles } from './styles';
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+
+const PressableAnimated = Animated.createAnimatedComponent(Pressable);
+
+import { THEME } from "../../styles/theme";
+import { styles } from "./styles";
 
 const TYPE_COLORS = {
   EASY: THEME.COLORS.BRAND_LIGHT,
   HARD: THEME.COLORS.DANGER_LIGHT,
   MEDIUM: THEME.COLORS.WARNING_LIGHT,
-}
+};
 
-type Props = TouchableOpacityProps & {
+type Props = PressableProps & {
   title: string;
   isChecked?: boolean;
   type?: keyof typeof TYPE_COLORS;
-}
+};
 
-export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Props) {
+export function Level({
+  title,
+  type = "EASY",
+  isChecked = false,
+  ...rest
+}: Props) {
+  const scale = useSharedValue(1);
+  const checked = useSharedValue(1);
 
   const COLOR = TYPE_COLORS[type];
 
+  const animatedContainerStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+    backgroundColor: interpolateColor(
+      checked.value,
+      [0, 1],
+      ["transparent", COLOR]
+    ),
+  }));
+
+  const animatedTextStyle = useAnimatedStyle(() => ({
+    color: interpolateColor(
+      checked.value,
+      [0, 1],
+      [COLOR, THEME.COLORS.GREY_100]
+    ),
+  }));
+
+  function onPressIn() {
+    scale.value = withSpring(1.1);
+  }
+
+  function onPressOut() {
+    scale.value = withSpring(1);
+  }
+
+  useEffect(() => {
+    checked.value = withSpring(isChecked ? 1 : 0);
+  }, [isChecked]);
+
   return (
-    <TouchableOpacity {...rest}>
-      <View style={
-        [
-          styles.container,
-          { borderColor: COLOR, backgroundColor: isChecked ? COLOR : 'transparent' }
-        ]
-      }>
-        <Text style={
-          [
-            styles.title,
-            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR }
-          ]}>
-          {title}
-        </Text>
-      </View>
-    </TouchableOpacity>
+    <PressableAnimated
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
+      style={[styles.container, { borderColor: COLOR }, animatedContainerStyle]}
+      {...rest}
+    >
+      <Animated.Text style={[styles.title, animatedTextStyle]}>
+        {title}
+      </Animated.Text>
+    </PressableAnimated>
   );
 }
