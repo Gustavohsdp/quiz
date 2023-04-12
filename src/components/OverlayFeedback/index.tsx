e73c443484c867af1a754c847ebeddfaa4161209
Gustavo Henrique <gustavohsdp@gmail.com>
2023-04-12 09:03:30 -0300

    feat: :sparkles: Create component OverlayFeedback and apply animations

---
+++ src/components/OverlayFeedback/index.tsx
@@ -0,0 +1,54 @@
+import { BlurMask, Canvas, Rect } from "@shopify/react-native-skia";
+import { useEffect } from "react";
+import { useWindowDimensions } from "react-native";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
+import { THEME } from "../../styles/theme";
+
+const STATUS = [
+  "transparent",
+  THEME.COLORS.BRAND_LIGHT,
+  THEME.COLORS.DANGER_LIGHT,
+];
+
+type Props = {
+  status: number;
+};
+
+export function OverlayFeedback({ status }: Props) {
+  const opacity = useSharedValue(0);
+
+  const color = STATUS[status];
+
+  const { height, width } = useWindowDimensions();
+
+  const styleAnimated = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
+  useEffect(() => {
+    opacity.value = withSequence(
+      withTiming(1, { duration: 400, easing: Easing.bounce }),
+      withTiming(0)
+    );
+  }, [status]);
+
+  return (
+    <Animated.View
+      style={[{ height, width, position: "absolute" }, styleAnimated]}
+    >
+      <Canvas style={{ flex: 1 }}>
+        <Rect x={0} y={0} width={width} height={height} color={color}>
+          <BlurMask blur={50} style="inner" />
+        </Rect>
+      </Canvas>
+    </Animated.View>
+  );
+}
--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -23,6 +23,7 @@ import Animated, {
 import { ConfirmButton } from "../../components/ConfirmButton";
 import { Loading } from "../../components/Loading";
 import { OutlineButton } from "../../components/OutlineButton";
+import { OverlayFeedback } from "../../components/OverlayFeedback";
 import { ProgressBar } from "../../components/ProgressBar";
 import { Question } from "../../components/Question";
 import { QuizHeader } from "../../components/QuizHeader";
@@ -41,6 +42,7 @@ export function Quiz() {
   const [points, setPoints] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [statusReply, setStatusReply] = useState(0);
   const [quiz, setQuiz] = useState<QuizProps>({} as QuizProps);
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(
     null
@@ -91,8 +93,10 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setStatusReply(1);
       setPoints((prevState) => prevState + 1);
     } else {
+      setStatusReply(2);
       shakeAnimation();
     }
 
@@ -212,6 +216,7 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
+      <OverlayFeedback status={statusReply} />
       <Animated.View style={fixedProgressBarStyles}>
         <Text style={styles.title}>{quiz.title}</Text>
 
